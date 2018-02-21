f288c15ba369667ccde50fc2b75ab69edaa0aedf
Suresh Kumar. Alagarswamy <skalagar@oradev.oraclecorp.com>
2018-02-21 16:09:32 +0530

    Added base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { CustomerComponent } from './customer/customer.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopNavbarComponent,
+    CustomerComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: '', component: HomeComponent},
+      {path: 'customer', component: CustomerComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
